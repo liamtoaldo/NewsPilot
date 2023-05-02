0bc28d33a84e950a66db321e05b4dd0739736283
Gyro7 <liamtoaldo@gmail.com>
2023-05-02 18:07:53 +0200

    Added appbar

--- app/src/main/java/com/liam/newspilot/MainActivity.java
+++ app/src/main/java/com/liam/newspilot/MainActivity.java
@@ -1,14 +1,20 @@
 package com.liam.newspilot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar appbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.appbar);
+        setSupportActionBar(appbar);
     }
 }
