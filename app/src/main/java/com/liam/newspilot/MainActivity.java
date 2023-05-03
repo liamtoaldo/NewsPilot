eae41f7c7e60df528c9ef455794c1cbd946d2580
Gyro7 <liamtoaldo@gmail.com>
2023-05-03 12:41:44 +0200

    Added search bar.

--- app/src/main/java/com/liam/newspilot/MainActivity.java
+++ app/src/main/java/com/liam/newspilot/MainActivity.java
@@ -2,11 +2,14 @@ package com.liam.newspilot;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,8 +31,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        //Put menu on the bar
         MenuInflater inflater=getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
+
+        //Create listener for search bar
+        MenuItem.OnActionExpandListener onActionExpandListener = new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                return true;
+            }
+        };
+
+        menu.findItem(R.id.search).setOnActionExpandListener(onActionExpandListener);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setQueryHint("Search topic...");
+
         return true;
     }
 }
